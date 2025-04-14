eb772922c999e87cc295a791c0e3621993e7eee2
Dylan Forbes <dylandforbes@gmail.com>
2025-04-14 11:45:06 -0700

    show the current player name during the play phase

--- src/page/Game/BoardView/BoardView.tsx
+++ src/page/Game/BoardView/BoardView.tsx
@@ -3,12 +3,18 @@ import BoardControls from './BoardControls';
 import PlayerHand from './PlayerHand';
 import Results from './Results';
 import { useGameplayStore } from '@/gameplay/store' ;
+import { getPlayerWithId } from '@/gameplay/state/Player';
 
 const BoardView = () => {
   const { gameState } = useGameplayStore();
-  const { phase } = gameState!;
+  const { phase, players, playPhaseActivePlayerId: playerId } = gameState!;
   return (
     <div>
+      { phase === 'play' && (
+        <div>
+          current player: { getPlayerWithId(players, playerId).name }
+        </div>
+      ) }
       <GameBoard />
       { ['setup', 'play'].includes(phase) && <BoardControls /> }
       <PlayerHand />
