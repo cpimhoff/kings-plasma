3ad9bfc56c312b925c074b812f705d4551703e38
Dylan Forbes <dylandforbes@gmail.com>
2025-04-20 14:15:10 -0700

    fix card library counts

--- src/page/Play/PlayerSetup/CardLibrary.tsx
+++ src/page/Play/PlayerSetup/CardLibrary.tsx
@@ -15,17 +15,21 @@ const CardLibrary = () => {
     <div className="flex flex-col">
       <h2> available cards </h2>
       <div className="flex flex-wrap justify-center gap-3 bg-slate-300 p-2">
-        {cardLibrary.map((card, cardIdx) => (
-          <SelectableCardWrapper
-            key={cardIdx}
-            count={deck[cardIdx] ?? 0}
-            maxCount={card.isLegendary ? 1 : 3}
-            enabled={!isDeckFull && (deck[cardIdx] ?? 0) < (card.isLegendary ? 1 : 3)}
-            onClick={() => addCardToDraftPlayerDeck(cardIdx)}
-          >
-            <FullCard card={card} color={'var(--player-color)'} />
-          </SelectableCardWrapper>
-        ))}
+        {cardLibrary.map((card, cardIdx) => {
+          const numInDeck = deck[cardIdx] ?? 0;
+          const maxAllowedInDeck = card.isLegendary ? 1 : 3;
+          return (
+            <SelectableCardWrapper
+              key={cardIdx}
+              count={maxAllowedInDeck - numInDeck}
+              maxCount={maxAllowedInDeck}
+              enabled={!isDeckFull && numInDeck < maxAllowedInDeck}
+              onClick={() => addCardToDraftPlayerDeck(cardIdx)}
+            >
+              <FullCard card={card} color={'var(--player-color)'} />
+            </SelectableCardWrapper>
+          );
+        })}
       </div>
     </div>
   );
